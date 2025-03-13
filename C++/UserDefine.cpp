dafbc850b7da1896e06b154139f2bf1244aee76f
anujd4348 <anujd4348@gmail.com>
2025-03-13 03:54:26 +0000

    user define is done

--- C++/UserDefine.cpp
+++ C++/UserDefine.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 
-void happyBirthday(a, b){
-    int c = a + b;
+// void happyBirthday(int a, int b){
+//     std::cout << "Happy Birthday!" << std::endl;
+//     std::cout << "You are " << a+b << " years old!" << std::endl;
+// }
 
-}
+// int main(){
+
+//     // function = block of reusable code
+//     happyBirthday(15, 15);
+//     happyBirthday(10, 12);
+
+//     return 0;
+// }
+
+void happyBirthday(std::string name, int age);
 
 int main(){
 
-    // function = block of reusable code
-    happyBirthday(15, 15);
+    std::string name = "bro";
+    int age = 16;
+
+    happyBirthday(name, age);
 
     return 0;
+}
+
+void happyBirthday(std::string name, int age){
+    std::cout<<"happy birthday to " << name << age << '\n';
+    std::cout<<"happy birthday to " << name << age << '\n';
+    std::cout<<"happy birthday to dear " << name << age << '\n';
+    std::cout<<"happy birthday to " << name << age << '\n';
 }
